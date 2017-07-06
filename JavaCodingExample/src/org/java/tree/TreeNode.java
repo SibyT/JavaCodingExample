84ad076e8188a00a28b943f2615a891e1d1a375b
sibs <sibythomas27@gmail.com>
2017-07-06 10:47:53 +0200

    a tree node

---
+++ JavaCodingExample/src/org/java/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package org.java.tree;
+/**
+ * Definition for a binary tree node.
+ * @author SibyThomas
+ *
+ */
+public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	 }
